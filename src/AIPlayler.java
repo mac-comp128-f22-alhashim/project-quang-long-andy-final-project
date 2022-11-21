087d4eb1377922a360919145d07114cf89ca3aaf
Quang Duong Nguyen <quanguyendg@gmail.com>
2022-11-21 13:58:41 -0600

    change the scanner input

---
+++ src/AIPlayler.java
@@ -0,0 +1,13 @@
+public class AIPlayler {
+    
+    private Board board;
+
+    public AIPlayler(Board board) {
+        this.board = board;
+    }
+
+    private void move() {
+        
+    }
+    
+}
--- src/Simple_TicTacToe.java
+++ src/Simple_TicTacToe.java
@@ -29,9 +29,8 @@ class Simple_TicTacToe {
                 System.out.println("Player 1's (O) turn");
 
             do {    // while loop to check for invalid input
-                System.out.print("What is your i (1-3 only):");
+                System.out.print("What is your move (row[1-3] column[1-3]): ");
                 valueI = scanner.nextInt();
-                System.out.print("What is your j (1-3 only):");
                 valueJ = scanner.nextInt();
                 System.out.println();
             } while (!board.playerChoose(isPlayer0, valueI, valueJ)); // check for invalid input
