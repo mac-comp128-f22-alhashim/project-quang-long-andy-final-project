eee34e46ee18948908c4b1831b40940f5f1cfd21
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2022-11-21 22:09:17 -0600

    Done with the Advanced XO
    Given the size of the board (size must be 5 or larger), players can now play with each other.
    The win condition is 5 identical marks in a row.

---
+++ src/AdvancedXO/UnitTest.java
@@ -0,0 +1,124 @@
+package AdvancedXO;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class UnitTest {
+    @Test
+    public void testWinConEmpty() {
+        BigBoard board = new BigBoard(4);
+        assertEquals(-1, board.checkWin());
+    }
+    @Test 
+    public void testSize() {
+        BigBoard board = new BigBoard(5);
+        assertEquals(5, board.getSize());
+    }
+
+    @Test
+    public void testVerticalWinCon() {
+        BigBoard board = new BigBoard(12);
+        board.playerChoose(true, 2, 1);
+        board.playerChoose(true, 3, 1);
+        board.playerChoose(true, 5, 1);
+        board.playerChoose(true, 6, 1);
+        board.playerChoose(true, 4, 1);
+        assertEquals(0, board.checkWin());
+    }
+    
+    @Test
+    public void testVerticalWinCon2() {
+        BigBoard board = new BigBoard(12);
+        board.playerChoose(true, 8, 12);
+        board.playerChoose(true, 7, 12);
+        board.playerChoose(true, 9, 12);
+        board.playerChoose(true, 11, 12);
+        board.playerChoose(true, 12, 12);
+        assertEquals(-1, board.checkWin());
+    }
+    @Test 
+    public void testVerticalWinCon3() {
+        BigBoard board = new BigBoard(5);
+        board.playerChoose(true, 5, 4);
+        assertEquals(4, board.getRecentI());
+        assertEquals(3, board.getRecentJ());
+        board.playerChoose(true, 3, 4);
+        board.playerChoose(true, 1, 4);
+        board.playerChoose(true, 2, 4);
+        board.playerChoose(false, 5, 5);
+        board.playerChoose(true, 4, 4);
+        // assertEquals(4, board.getRecentJ());
+        assertEquals(0, board.checkWin());
+    }
+
+    @Test
+    public void testHorizontalWinCon() {
+        BigBoard board = new BigBoard(5);
+        board.playerChoose(false, 1, 3);
+        board.playerChoose(false, 1, 5);
+        board.playerChoose(false, 1, 4);
+        board.playerChoose(true, 2, 2);
+        board.playerChoose(false, 1, 1);
+        board.playerChoose(false, 1, 2);
+        assertEquals(1, board.checkWin());
+    }
+
+    @Test
+    public void testHorizontalWinCon2() {
+        BigBoard board = new BigBoard(5);
+        int arr[] = {1,2,3,4,5};
+        board.playerChoose(false, 3, 4);
+        for (int j=0; j<5; j++) {
+            board.playerChoose(true, 2, arr[j]);
+        }
+        assertEquals(0, board.checkWin());
+    }
+
+    @Test 
+    public void testDiagonal() {
+        BigBoard board = new BigBoard(20);
+        board.playerChoose(true, 20, 20);
+        board.playerChoose(true, 19, 19);
+        board.playerChoose(true, 18, 18);
+        board.playerChoose(true, 17, 17);
+        board.playerChoose(true, 16, 16);
+        assertEquals(0, board.checkWin());
+
+        board.unmarked(16, 16);
+        assertEquals(-1, board.checkWin());
+
+        board.playerChoose(false, 5, 5);
+        board.playerChoose(false, 4, 6);
+        board.playerChoose(false, 3, 7);
+        board.playerChoose(false, 2, 8);
+        board.playerChoose(false, 1, 9);
+        assertEquals(1, board.checkWin());
+    }
+
+    // -----
+    // XXXX-
+    // -OXO-
+    // -XOO-
+    // -----
+    @Test
+    public void generalTest() {
+        BigBoard board = new BigBoard(5);
+        int[][] input = {{0,2,2},{1,4,3},{0,2,1},{1,4,4},{0,2,3},{1,3,2},{1,3,4},{0,3,3},{0,4,2},{0,2,4}};
+        boolean bool = false;
+        for (int i = 0; i<input.length;i++) {
+            if (input[i][0] == 0) {
+                bool = true;
+            }
+            else if (input[i][0] == 1) {
+                bool = false;
+            }
+            board.playerChoose(bool, input[i][1], input[i][2]);
+        }
+        board.getPrinted();
+        assertEquals(-1, board.checkWin());
+
+        board.playerChoose(true, 2, 5);
+        assertEquals(0, board.checkWin());
+    }
+}
---
+++ src/AdvancedXO/main.java
@@ -0,0 +1,61 @@
+package AdvancedXO;
+
+import java.util.Scanner;
+
+public class main {
+    private static BigBoard board;
+    
+    public static void main(String[] args) {
+        board = new BigBoard(7);
+        gameRun();
+    }
+
+    public static void gameRun() {
+        Boolean isPlayer0 = true;
+        Scanner scanner = new Scanner(System.in);
+        Integer valueI = 0;
+        Integer valueJ = 0;
+        for (int i = 0; i < board.getSize()*board.getSize(); i++) {
+            // Clear screen gimmick
+            System.out.print("\033[H\033[2J");  
+            System.out.flush(); 
+            board.getPrinted();
+            System.out.println();
+            if (isPlayer0)
+                System.out.println("Player 0's (X) turn:");
+            else
+                System.out.println("Player 1's (O) turn");
+
+            do {    // while loop to check for invalid input
+                System.out.print("What is your i (1-size only):");
+                valueI = scanner.nextInt();
+                System.out.print("What is your j (1-size only):");
+                valueJ = scanner.nextInt();
+                System.out.println();
+            } while (!board.playerChoose(isPlayer0, valueI, valueJ)); // check for invalid input
+
+            // if playerX won
+            if (board.checkWin() == 0) {
+                System.out.print("\033[H\033[2J");  
+                System.out.flush();
+                board.getPrinted();
+                System.out.println("Player 0 (X) won");
+                break;
+            }
+
+            // if playerO won
+            else if (board.checkWin() == 1) {
+                System.out.print("\033[H\033[2J");  
+                System.out.flush();
+                board.getPrinted();
+                System.out.println("Player 1 (O) won");
+                break;
+            }
+            // if not then continue
+            isPlayer0 = !isPlayer0; // Switch turn
+        }
+        scanner.close();
+    }
+
+    
+}
