ed5cfe639bd8b49fd56812292384b833f461d2d9
Quang Duong Nguyen <quanguyendg@gmail.com>
2022-11-24 19:47:22 -0600

    Update AIPlayler.java

--- src/AIPlayler.java
+++ src/AIPlayler.java
@@ -6,8 +6,17 @@ public class AIPlayler {
         this.board = board;
     }
 
+    private int[][] preferredMoves = {
+        {1, 1}, {0, 0}, {0, 2}, {2, 0}, {2, 2},
+        {0, 1}, {1, 0}, {1, 2}, {2, 1}};
+
     private void move() {
-        
+        for (int[] move : preferredMoves) {
+            if (board[move[0]][move[1]] == '-') {
+                return;
+            }
+    
+        }
     }
     
 }
