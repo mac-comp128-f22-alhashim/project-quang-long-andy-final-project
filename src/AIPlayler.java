f320c4bdcbd4c03d88874bac21ecfba452f51bdb
Quang Duong Nguyen <quanguyendg@gmail.com>
2022-11-28 22:58:41 -0600

    Update AIPlayler.java

--- src/AIPlayler.java
+++ src/AIPlayler.java
@@ -7,8 +7,8 @@ public class AIPlayler {
     }
 
     private int[][] preferredMoves = {
-        {1, 1}, {0, 0}, {0, 2}, {2, 0}, {2, 2},
-        {0, 1}, {1, 0}, {1, 2}, {2, 1}};
+        {2, 2}, {1, 1}, {1, 3}, {3, 1}, {3, 3},
+        {1, 2}, {2, 1}, {2, 3}, {3, 2}};
 
     public int[] move() {
         for (int[] move : preferredMoves) {
